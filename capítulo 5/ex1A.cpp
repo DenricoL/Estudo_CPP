ea06af9f2b82470cdb58dea87884399629134e2c
Douglas <163703664+DenricoL@users.noreply.github.com>
2025-02-01 22:15:44 -0300

    Add files via upload

---
+++ capítulo 5/ex1A.cpp
@@ -0,0 +1,25 @@
+//exc a capitulo 5 - quadrados de 15 a 200
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main (void)
+{
+  setlocale (LC_ALL, "Portuguese");
+  int x;
+
+  x = 15;
+
+  while (x <= 200)
+  {
+    int quad = pow (x,2);
+    cout << "O quadrado de " << x << " é = " << quad << endl;
+    ++x;
+  }
+  cout << endl;
+  cout << "Tecle <ENTER> para encerrar...";
+  cin.get();
+  return 0;
+}
---
+++ capítulo 5/exT/exT_2.c
@@ -0,0 +1,14 @@
+//cap 5 ex T
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main (void)
+{
+
+  for (int n = 15, n <= 200, n = n + 3)
+    cout << pow(n,2);
+  return 0;
+}
